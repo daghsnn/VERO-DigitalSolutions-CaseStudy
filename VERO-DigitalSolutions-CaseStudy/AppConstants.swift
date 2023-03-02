abdea3de98be68623966859ae1867b6373b0a628
Hasan Dağ <daghsnn@gmail.com>
2023-03-02 19:34:14 +0300

    Added Constants problem

---
+++ VERO-DigitalSolutions-CaseStudy/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  VERO-DigitalSolutions-CaseStudy
+//
+//  Created by Hasan Dag on 2.03.2023.
+//
+
+import Foundation
+
+enum Constants:String{
+    case BASIC_TOKEN = "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz"
+    case BASE_URL = "https://api.baubuddy.de"
+    case BEARER = "%@ %@"
+}
